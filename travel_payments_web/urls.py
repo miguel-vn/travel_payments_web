3a1b3bbec294c36a923f75e621a2cb6d5050a82e
m.novikov <m.novikov@x5.ru>
2021-08-21 14:57:46 +0300

    Added social auth urls namespace

--- travel_payments_web/urls.py
+++ travel_payments_web/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('payments_logic.urls')),
+    path('', include('social_django.urls', namespace='social'))
+
 ]
